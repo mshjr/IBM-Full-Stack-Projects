aa5514c985eb931f5d7bff001aa06de82644cbf5
Mike Hart <mike.s.hart.jr@gmail.com>
2024-09-10 10:24:16 -0400

    Add files via upload

---
+++ Intro to Web Dev/README.md
@@ -0,0 +1,8 @@
+# IBM-Introduction-to-Web-Development-with-HTML-CSS-JavaScript
+
+This is the final project of the course "Introduction to web development with HTML, CSS, Javascript" by IBM on Coursera Learning Platform.
+It is a single portfolio website created using HTML, CSS, Javascript.
+
+## Certification
+
+## Link of this webpage:
---
+++ Intro to Web Dev/scripts.js
@@ -0,0 +1,28 @@
+function addRecommendation() {
+  const newRecommendation = document.getElementById("new_recommendation").value;
+
+  if (newRecommendation.trim() === "") {
+    alert("Please enter a message.");
+    return;
+  }
+
+  const newRecElement = document.createElement("div");
+  newRecElement.classList.add("recommendation");
+
+  // Add the content to the new recommendation element
+  newRecElement.innerHTML = `<span>&#8220;</span> ${newRecommendation} <span>&#8221;</span>`;
+
+  // Add the new recommendation to the list of recommendations
+  document.getElementById("all_recommendations").appendChild(newRecElement);
+
+  // Show the popup confirmation message
+  const popup = document.getElementById("popup");
+  popup.style.display = "flex";
+
+  // Clear the input fields
+  document.getElementById("new_recommendation").value = "";
+}
+
+document.querySelector("#popup button").addEventListener("click", () => {
+  document.getElementById("popup").style.display = "none";
+});
